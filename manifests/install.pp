90727d73f62b474435fdc43976693c8109636472
Karl Vollmer <karl.vollmer@gmail.com>
2016-10-19 14:45:09 -0400

    Fix a problem where the top scope varliable name wasn't being used

--- manifests/install.pp
+++ manifests/install.pp
@@ -21,10 +21,10 @@ class gitlabr10khook::install inherits gitlabr10khook {
 
   ## Checkout the Gitlab Puppet webhook
   exec { 'gitlabr10khook-checkout-from-gitlab':
-    command => "git clone https://github.com/vollmerk/gitlab-puppet-webhook.git ${install}",
+    command => "git clone https://github.com/vollmerk/gitlab-puppet-webhook.git ${gitlabr10khook::install}",
     user    => 'root',
     require => Package['git'],
-    creates => $install,
+    creates => $gitlabr10khook::install,
     notify  => Exec['gitlabr10khook-update-python-daemon'],
   }
 
