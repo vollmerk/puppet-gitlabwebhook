aeb39698a57827218c1f8e5ae2ee552d1ca6d23c
Karl Vollmer <karl.vollmer@gmail.com>
2018-04-04 08:08:52 -0300

    Incorrect variable use :(

--- manifests/config.pp
+++ manifests/config.pp
@@ -20,7 +20,7 @@ class gitlabr10khook::config inherits gitlabr10khook {
     ensure  => file,
     mode    => '0640',
     owner   => 'root',
-    group   => $gitlabr10khook::group,
+    group   => $gitlabr10khook::server['group'],
     content => template('gitlabr10khook/webhook-puppet.erb'),
   }
 
@@ -32,16 +32,16 @@ class gitlabr10khook::config inherits gitlabr10khook {
   file { $logdir:
     ensure => directory,
     mode   => '0770',
-    owner  => $gitlabr10khook::user,
-    group  => $gitlabr10khook::group,
+    owner  => $gitlabr10khook::server['user'],
+    group  => $gitlabr10khook::server['group'],
   }
 
   # Make sure the log file exists and is writeable by the runner
   file { $logfile:
     ensure  => file,
     mode    => '0660',
-    owner   => $gitlabr10khook::user,
-    group   => $gitlabr10khook::group,
+    owner   => $gitlabr10khook::server['user'],
+    group   => $gitlabr10khook::server['group'],
     require => File[$logdir],
   }
 
